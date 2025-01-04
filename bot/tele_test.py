445e878a6108836de9b47a6f5d8a49d400bd8678
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2025-01-05 01:44:34 +0300

    clean up

--- bot/tele_test.py
+++
@@ -1,9 +0,0 @@
-import json
-
-PATH = "./data/test.json"
-
-with open(PATH, 'r') as js:
-    test = json.load(js)
-
-QUESTION_COUNT = len(test) 
-INGREDIENT_QUESTION_COUNT = len([item for item in test if item['ingredients']]) 
