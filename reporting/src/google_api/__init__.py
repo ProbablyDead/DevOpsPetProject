727c6dfb29112cb4a4749959f593d81ddf4b7a56
ProbablyDead <97610135+ProbablyDead@users.noreply.github.com>
2025-01-15 03:44:27 +0300

    move credentials file's path to env

--- reporting/src/google_api/__init__.py
+++ reporting/src/google_api/__init__.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 
 EMPTY_USER = "__empty__"
-CREDENTIALS_FILE = './src/google_api/secrets/credentials.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+CREDENTIALS_FILE = os.getenv('CREDENTIALS_FILE')
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 INGREDIENT_QUESTION_COUNT = int(os.getenv("INGREDIENT_QUESTION_COUNT"))
 
